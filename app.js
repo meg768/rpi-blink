ed6be8d0d99f6fdd2122ec96ed785ad64c9e2045
meg768 <magnus@egelberg.se>
2017-06-01 20:55:16 +0200

    -

--- app.js
+++ app.js
@@ -2,6 +2,18 @@
 
 var Gpio = require('pigpio').Gpio;
 
-var motor = new Gpio(10, {mode: Gpio.OUTPUT});
+var led = new Gpio(17, {mode: Gpio.OUTPUT});
 
 console.log('rpi-blink');
+
+
+var dutyCycle = 0;
+
+setInterval(function () {
+  led.pwmWrite(dutyCycle);
+
+  dutyCycle += 5;
+  if (dutyCycle > 255) {
+    dutyCycle = 0;
+  }
+}, 20);
