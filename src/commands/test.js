6b05811ffd59d6ee9ab16edfda6e492d2a162c7f
meg768 <magnus@egelberg.se>
2017-06-08 13:41:47 +0200

    -

--- src/commands/test.js
+++ src/commands/test.js
@@ -26,7 +26,7 @@ function NeopixelStrip(options) {
 	_this.fadeToColor = function(red, green, blue, steps) {
 
 		if (steps == undefined)
-			steps = 250;
+			steps = 201;
 
 		red    = parseInt(red);
 		green  = parseInt(green);
