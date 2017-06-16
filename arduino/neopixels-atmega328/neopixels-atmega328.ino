f29ec1f5f0feb0e17fc3705b78f27c8d625a5005
meg768 <magnus@egelberg.se>
2017-06-16 11:37:55 +0200

    -

--- arduino/neopixels-atmega328/neopixels-atmega328.ino
+++ arduino/neopixels-atmega328/neopixels-atmega328.ino
@@ -24,7 +24,7 @@
 const int ACK = 6;
 const int NAK = 21;
 
-const int PIN_LED_HEARTBEAT = 9;
+const int PIN_LED_HEARTBEAT = PB5;
 const int PIN_LED_ERROR     = 10;
 const int PIN_LED_BUSY      = 11;
 
@@ -34,8 +34,8 @@ const int CMD_INITIALIZE    = 0x10;  // size
 const int CMD_SET_COLOR     = 0x11;  // red, green, blue
 const int CMD_FADE_TO_COLOR = 0x12;  // red, green, blue
 const int CMD_WIPE_TO_COLOR = 0x13;  // red, green, blue, delay
-const int CMD_SET_PIXEL     = 0x14;  // 
-const int CMD_REFRESH       = 0x15;  // 
+const int CMD_SET_PIXEL     = 0x14;  //
+const int CMD_REFRESH       = 0x15;  //
 
 const int ERR_OK                = 0;
 const int ERR_INVALID_PARAMETER = 1;
@@ -60,7 +60,7 @@ class NeopixelStrip {
 
 
     public:
-        
+
         NeopixelStrip(int length) : _strip(length, NEOPIXEL_PIN, NEO_BRG + NEO_KHZ800) {
             _strip.begin();
         };
@@ -68,17 +68,17 @@ class NeopixelStrip {
         int numPixels() {
             return _strip.numPixels();
         }
-        
+
         void setColor(int red, int green, int blue) {
-        
+
             for (int i = 0; i < _strip.numPixels(); i++) {
                 _strip.setPixelColor(i, red, green, blue);
             }
-        
-            _strip.show();
-        }    
 
-        void setPixelColor(int i, int red, int green, int blue) {            
+            _strip.show();
+        }
+
+        void setPixelColor(int i, int red, int green, int blue) {
             _strip.setPixelColor(i, red, green, blue);
         }
 
@@ -87,49 +87,49 @@ class NeopixelStrip {
         }
 
         void wipeToColor(int red, int green, int blue, int wait) {
-        
+
             for (int i = 0; i < _strip.numPixels(); i++) {
                 _strip.setPixelColor(i, red, green, blue);
                 _strip.show();
                 delay(wait);
             }
         }
-    
+
         void fadeToColor(int red, int green, int blue, int numSteps) {
-        
-            
+
+
             int numPixels = _strip.numPixels();
-        
+
             for (int i = 0; i < numPixels; i++) {
                 uint32_t color = _strip.getPixelColor(i);
                 rgb[i].red   = (int)(uint8_t)(color >> 16);
                 rgb[i].green = (int)(uint8_t)(color >> 8);
                 rgb[i].blue  = (int)(uint8_t)(color);
             }
-        
+
             for (int step = 0; step < numSteps; step++) {
-        
+
                 for (int i = 0; i < numPixels; i++) {
-                    uint8_t pixelRed   = rgb[i].red   + (step * (red   - rgb[i].red))   / numSteps; 
+                    uint8_t pixelRed   = rgb[i].red   + (step * (red   - rgb[i].red))   / numSteps;
                     uint8_t pixelGreen = rgb[i].green + (step * (green - rgb[i].green)) / numSteps;
                     uint8_t pixelBlue  = rgb[i].blue  + (step * (blue  - rgb[i].blue))  / numSteps;
-            
+
                     _strip.setPixelColor(i, pixelRed, pixelGreen, pixelBlue);
                 }
-        
+
                 _strip.show();
-            
+
             }
-        
+
             for (int i = 0; i < numPixels; i++)
                 _strip.setPixelColor(i, red, green, blue);
-        
-            
+
+
             _strip.show();
-        
+
         }
-    
-        
+
+
     private:
         Adafruit_NeoPixel _strip;
 };
@@ -140,7 +140,7 @@ class NeopixelStrip {
 class LedLamp {
 
     public:
-        
+
         LedLamp(int pin = -1) {
 
             if (pin >= 0)
@@ -150,7 +150,7 @@ class LedLamp {
         void setPin(int pin) {
             _pin = pin;
             _state = LOW;
-            
+
             pinMode(pin, OUTPUT);
             digitalWrite(pin, _state);
         }
@@ -159,7 +159,7 @@ class LedLamp {
             _state = state ? HIGH : LOW;
             digitalWrite(_pin, _state);
         }
-        
+
         void toggleState() {
             setState(_state == LOW ? HIGH : LOW);
         }
@@ -182,7 +182,7 @@ static void *_app = NULL;
 
 class App {
 
-    
+
     public:
 
         App(int address, int stripLength) {
@@ -191,23 +191,23 @@ class App {
             _status = ACK;
             _address = address;
             _stripLength = stripLength;
-            
+
             _error.setPin(PIN_LED_ERROR);
             _heartbeat.setPin(LED_BUILTIN);
             _busy.setPin(PIN_LED_BUSY);
         }
-        
+
         void setup() {
             Wire.begin(_address);
             Wire.onReceive(App::receive);
-            Wire.onRequest(App::request);            
+            Wire.onRequest(App::request);
 
             _error.blink(2, 250);
 
              _strip = new NeopixelStrip(8);
-             _strip->setColor(0, 32, 0);            
+             _strip->setColor(0, 32, 0);
         }
-    
+
         void loop() {
             static uint32_t counter = 0;
 
@@ -215,28 +215,28 @@ class App {
 
             if ((counter % 1000) == 0)
                 _heartbeat.toggleState();
-    
+
         };
 
         static void receive(int bytes) {
             ((App *)_app)->onReceive(bytes);
         };
-        
+
         static void request() {
             ((App *)_app)->onRequest();
         };
 
-        int waitForAvailableBytes(int bytes) 
+        int waitForAvailableBytes(int bytes)
         {
             long timeout = millis() + 300;
-            
+
             for (;;) {
                 if (Wire.available() >= bytes) {
                     return true;
                 }
 
                 delay(100);
-                
+
                 if (millis() > timeout)
                     return false;
             }
@@ -251,7 +251,7 @@ class App {
 
             return true;
         };
-        
+
         int readRGB(int &red, int &green, int &blue) {
 
             if (!waitForAvailableBytes(3))
@@ -261,25 +261,25 @@ class App {
             blue = Wire.read();
             green = Wire.read();
 
-            return true;            
+            return true;
         };
 
         int onCommand(int command) {
-            
+
             switch (command) {
                 case CMD_INITIALIZE: {
                     int length = 0;
-        
+
                     if (!readByte(length))
                         return ERR_PARAMETER_MISSING;
-        
+
                     if (length < 0 || length > 240)
                         return ERR_INVALID_PARAMETER;
 
                     if (_strip != NULL) {
                         _strip->setColor(0, 0, 0);
-                    }                   
-                        
+                    }
+
                     delete _strip;
                     _strip = new NeopixelStrip(length);
 
@@ -292,7 +292,7 @@ class App {
                         return ERR_NOT_INITIALIZED;
 
                     int red = 0, green = 0, blue = 0, index = 0;
-    
+
                     if (!readByte(index))
                         return ERR_INVALID_PARAMETER;
 
@@ -316,46 +316,46 @@ class App {
 
                     if (_strip == NULL)
                         return ERR_NOT_INITIALIZED;
-                        
+
                     int red = 0, green = 0, blue = 0;
-    
+
                     if (!readRGB(red, green, blue))
                         return ERR_INVALID_PARAMETER;
-                    
+
                     _strip->setColor(red, green, blue);
 
                     break;
                 };
-        
+
                 case CMD_WIPE_TO_COLOR: {
                     if (_strip == NULL)
                         return ERR_NOT_INITIALIZED;
 
                     int red = 0, green = 0, blue = 0, delay = 0;
-    
+
                     if (!readRGB(red, green, blue))
                         return ERR_INVALID_PARAMETER;
-                    
+
                     if (!readByte(delay))
                         return ERR_INVALID_PARAMETER;
-    
+
                     _strip->wipeToColor(red, green, blue, delay);
 
                     break;
                 }
-    
+
                 case CMD_FADE_TO_COLOR: {
                     if (_strip == NULL)
                         return ERR_NOT_INITIALIZED;
 
                     int red = 0, green = 0, blue = 0, steps = 0;
-    
+
                     if (!readRGB(red, green, blue))
                         return ERR_INVALID_PARAMETER;
-                    
+
                     if (!readByte(steps))
                         return ERR_INVALID_PARAMETER;
-    
+
                     _strip->fadeToColor(red, green, blue, steps);
 
                     break;
@@ -367,16 +367,16 @@ class App {
             };
 
             return ERR_OK;
-    
+
         }
-        
+
 
 
         void onReceive(int bytes) {
-        
+
             while (Wire.available()) {
                 int command = ERR_INVALID_COMMAND, error = ERR_OK;
-                
+
                 if (readByte(command)) {
                     _status = NAK;
 
@@ -385,18 +385,18 @@ class App {
                     _busy.setState(LOW);
 
                     _status = ACK;
-                
+
                 }
 
                 if (error > 0) {
                     Wire.flush();
                     _error.blink(error);
                 }
-        
+
             }
-        
-            
-        
+
+
+
         }
 
         void onRequest() {
@@ -418,7 +418,7 @@ class App {
 
 
 // Define a strip at the specified address and with a default length
-App app(0x26, 8);
+App app(0x26, 10);
 
 void setup()
 {
@@ -426,19 +426,7 @@ void setup()
 }
 
 
-void loop() 
+void loop()
 {
     app.loop();
 }
-
-
-
-
-
-
-
-
-
-
-
-
