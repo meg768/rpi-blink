d3196cbe575ca1afbcb35d70dbbfed274c9b770a
meg768 <magnus@egelberg.se>
2017-06-21 23:34:12 +0200

    -

--- src/commands/test.js
+++ src/commands/test.js
@@ -276,6 +276,9 @@ module.exports.handler = function(args) {
 			return strip.pause(2000);
 		})
 
+		promise.then(function() {
+			return strip.setColor(0, 255, 255, 0, 0);
+		})
 		.then(function() {
 			return strip.fadeToColor(0, 255, 0, 0, 0, 1000);
 		})
