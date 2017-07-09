cd201faeff74e03027247e2ec72f9041f1a0aa3e
meg768 <meg768@users.noreply.github.com>
2017-07-09 23:07:17 +0200

    -

--- src/commands/test.js
+++ src/commands/test.js
@@ -337,7 +337,7 @@ module.exports.handler = function(args) {
 
 					var promise = Promise.resolve();
 
-					for (var i = 0; i < 20; 1++) {
+					for (var i = 0; i < 20; i++) {
 						promise = promise.then(function() {
 							console.log('KALLE', i)
 							return setColor(bar[i % 4], random([128, 255, 0], random([128, 255, 0]), random([128, 255, 0]));
