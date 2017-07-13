e0a9c29a4e8a2892c4c2151c36da3e2394d6fee2
meg768 <meg768@users.noreply.github.com>
2017-07-13 16:32:17 +0200

    -

--- src/commands/test.js
+++ src/commands/test.js
@@ -39,7 +39,7 @@ module.exports.handler = function(args) {
 		function setColor(bar, red, green, blue, wait) {
 
 			if (wait == undefined)
-				wait = 300;
+				wait = 400;
 
 			return new Promise(function(resolve, reject) {
 				bar.setColor(red, green, blue, 200).then(function() {
